c058c0d58dd01493f4de6b6292fc16767c534256
aokuznecova <aokuznecova@2100.gosniias.ru>
2024-01-15 15:59:03 +0300

    Added button sound + more beautiful fonts

--- Assets/_Scripts/AudioManager.cs
+++ Assets/_Scripts/AudioManager.cs
@@ -2,6 +2,19 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    
+    [SerializeField] private AudioSource _backgroundSoundtrack;
+    [SerializeField] private AudioSource _clickSound;
 
+    public void PlaySound(string soundName)
+    {
+        switch (soundName)
+        {
+            case "Click":
+                _clickSound.Play();
+                break;
+            case "Background":
+                _backgroundSoundtrack.Play();
+                break;
+        }
+    }
 }
