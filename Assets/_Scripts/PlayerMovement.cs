fce7c906204624b5757fa5bc7a149dea89c3ba7c
aokuznecova <aokuznecova@2100.gosniias.ru>
2024-01-22 19:14:52 +0300

    Added localization and instructions

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{  
+{
     [SerializeField] private float playerSpeed = 7f;
     [SerializeField] private float maximumPlayerSpeed;
     [SerializeField] private float playerSpeedIncreaseRate;
@@ -10,31 +10,32 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 positiveRotation = new Vector3(0, 90, 0);
     private Vector3 negativeRotation = new Vector3(0, -90, 0);
     private CharacterController cc;
-    
+
     private void Awake()
     {
         cc = GetComponent<CharacterController>();
-        inputManager.OnStartTouch += Rotate;
+        //inputManager.OnStartTouch += Rotate;
     }
 
     private void Update()
     {
         Physics.SyncTransforms();
         cc.Move(transform.forward * playerSpeed * Time.deltaTime);
-        
-        if(playerSpeed < maximumPlayerSpeed)
+
+        if (playerSpeed < maximumPlayerSpeed)
         {
             playerSpeed += Time.deltaTime * playerSpeedIncreaseRate;
         }
 
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
             transform.Rotate(negativeRotation);
-        else if(Input.GetKeyDown(KeyCode.D))
+        else if (Input.GetKeyDown(KeyCode.D)|| Input.GetKeyDown(KeyCode.RightArrow))
             transform.Rotate(positiveRotation);
     }
+
     private void OnDestroy()
     {
-       inputManager.OnEndTouch -= Rotate;
+        //inputManager.OnEndTouch -= Rotate;
     }
 
     // private void OnEnable()
@@ -48,11 +49,22 @@ public class PlayerMovement : MonoBehaviour
 
     public void Rotate(Vector2 screenPosition, float time)
     {
-        if (screenPosition.x >  Screen.width * 0.67)
+        if (screenPosition.x > Screen.width * 0.67)
         {
             transform.Rotate(positiveRotation);
         }
         else if (screenPosition.x < Screen.width * 0.33)
+        {
+            transform.Rotate(negativeRotation);
+        }
+    }
+    public void Rotate(bool positiveRot)
+    {
+        if (positiveRot)
+        {
+            transform.Rotate(positiveRotation);
+        }
+        else
         {
             transform.Rotate(negativeRotation);
         }
