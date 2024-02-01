0f092d7106bedd9737751027f6cf677ebfc8e72a
Neeraxed <83071421+Neeraxed@users.noreply.github.com>
2024-02-01 22:48:23 +0300

    Made sound turn off while ad playing

--- Assets/_Scripts/AudioManager.cs
+++ Assets/_Scripts/AudioManager.cs
@@ -5,6 +5,7 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioSource _backgroundSoundtrack;
     [SerializeField] private AudioSource _clickSound;
 
+    private static bool IsSoundOff;
     public void PlaySound(string soundName)
     {
         switch (soundName)
@@ -21,5 +22,11 @@ public class AudioManager : MonoBehaviour
     public void StopPlayEverything()
     {
         AudioListener.pause = !AudioListener.pause;
+        IsSoundOff = !IsSoundOff;
+    }
+    public void StopPlayEverything(bool par)
+    {
+        AudioListener.pause = par;
+        IsSoundOff = par;
     }
 }
